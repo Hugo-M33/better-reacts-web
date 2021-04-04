4176c2d10536df7b5c2552d155dd712429a00cd3
Hugo Martin <hugo.martin.duloz@gmail.com>
2021-04-04 21:46:46 +0200

    trying POST function

---
+++ functions/getSingle.js
@@ -0,0 +1,21 @@
+const admin = require("firebase-admin");
+const querystring = require("querystring");
+
+const serviceAccount = require("../serviceAccount.json");
+if (!admin.apps.length) {
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+  });
+}
+exports.handler = async function (event, context) {
+    // Only allow POST
+  if (event.httpMethod !== "POST") {
+    return { statusCode: 405, body: "Method Not Allowed" };
+  }
+  const params = querystring.parse(event.body);
+
+    return {
+        statusCode: 200,
+        body: JSON.stringify(params),
+      };
+    };
