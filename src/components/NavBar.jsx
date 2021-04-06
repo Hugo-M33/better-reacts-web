1caf9c0114717550cb47584b73b2a7f499d26b6b
Hugo Martin <hugo.martin.duloz@gmail.com>
2021-04-06 22:50:36 +0200

    added swag message

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,4 +1,6 @@
 import styled from 'styled-components'
+import { motion, AnimateSharedLayout } from 'framer-motion'
+import { useState } from 'react'
 
 const StyledNav = styled.nav`
     height: 100px;
@@ -9,27 +11,51 @@ const StyledNav = styled.nav`
     display: flex;
     justify-content: space-around;
     align-items: center;
+    z-index:3;
     
 `
 
-const Menu = styled.div`
+const Menu = styled.ul`
     width: 60%;
     display: flex;
     justify-content: space-around;
     align-items: center;
 `
 
+const spring = {
+  type: "spring",
+  stiffness: 500,
+  damping: 30
+};
 
-
+const Item = ({ isSelected, onClick, children }) => {
+    return (
+      <li onClick={onClick} style={{listStyle:"none", position: "relative", cursor: "pointer"}}>
+          <h2 style={{color: isSelected? "white": "black"}}>{children}
+        {isSelected && (
+          <motion.div
+            layoutId="backgroundNav"
+            style={{background: "#B73254", opacity: .75, borderRadius: 15, padding: "2%", position: "absolute", top: 10, bottom: 10, left: -10, right: -10, zIndex:-1}}
+            initial={false}
+            transition={spring}
+          ></motion.div>
+          )}
+          </h2>
+      </li>
+    );
+  }
 
 const NavBar = () => {
+    const [pages, setPages] = useState(["Home", "Submit"])
+    const [selected,setSelected] = useState(pages[0])
     return (
-        <StyledNav>
-            <Menu>
-                <div>Home</div>
-                <div>Submit</div>
-            </Menu>
-        </StyledNav>
+        <AnimateSharedLayout>
+            <StyledNav>
+                <Menu>
+                    {pages.map(p => <Item key={p} isSelected={p == selected} onClick={() => setSelected(p)}>{p}</Item>)}
+                </Menu>
+            </StyledNav>
+        </AnimateSharedLayout>
     )
 }
 
